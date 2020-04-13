e831eb7d2e4bbccee9c829d0249ce04445c367d0
Jeff W <Jeff@fraction.rent>
2020-04-13 06:01:45 -0400

    Updates

---
+++ precache-manifest.e9a14809037031a97f16972578b8b433.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd20d372213497c832afb334817eee3",
+    "url": "/insta_emails_new/index.html"
+  },
+  {
+    "revision": "88c4a7c45efe9fc1ae33",
+    "url": "/insta_emails_new/static/css/main.022d19ec.chunk.css"
+  },
+  {
+    "revision": "d7c4814a3233249d750a",
+    "url": "/insta_emails_new/static/js/2.53e9c6d1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/insta_emails_new/static/js/2.53e9c6d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88c4a7c45efe9fc1ae33",
+    "url": "/insta_emails_new/static/js/main.6a645098.chunk.js"
+  },
+  {
+    "revision": "1f9579bf7dcb9c968844",
+    "url": "/insta_emails_new/static/js/runtime-main.8f5151f1.js"
+  }
+]);
